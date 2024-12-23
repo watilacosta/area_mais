52617112ff4739b14bd111b1da033ba8bcdc267e
Watila Costa <watilafreire26@gmail.com>
2024-12-22 21:15:57 -0300

    list of non members users

--- app/models/exclusive_area.rb
+++ app/models/exclusive_area.rb
@@ -1,6 +1,9 @@
 class ExclusiveArea < ApplicationRecord
-  belongs_to :owner, class_name: "User", foreign_key: "owner_id"
-  has_many :memberships
+  belongs_to :owner, class_name: "User", foreign_key: "owner_id", inverse_of: :owned_areas
+  has_many :memberships, dependent: :destroy
   has_many :members, through: :memberships, source: :user
-  has_many :contents
+
+  validates :name, presence: true, uniqueness: { scope: :owner_id }
+  validates :description
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
 end
